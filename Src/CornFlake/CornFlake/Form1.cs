e9320bff38218dfb2f7b3f8757f1dbac2a178e57
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-09-03 07:54:30 +0200

    Add a link

---
+++ Src/CornFlake/CornFlake/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CornFlake
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("user32.dll")]
+        public static extern bool GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+        public Bitmap picture;
+        private static int width = Screen.PrimaryScreen.Bounds.Width;
+        private static int height = Screen.PrimaryScreen.Bounds.Height;
+        public static int[] wd = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+        public static int[] wu = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+        public static bool[] ws = { false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false };
+        static void valchanged(int n, bool val)
+        {
+            if (val)
+            {
+                if (wd[n] <= 1)
+                {
+                    wd[n] = wd[n] + 1;
+                }
+                wu[n] = 0;
+            }
+            else
+            {
+                if (wu[n] <= 1)
+                {
+                    wu[n] = wu[n] + 1;
+                }
+                wd[n] = 0;
+            }
+            ws[n] = val;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                picture = new Bitmap("cornflake.png");
+                picture = ResizeBitmap(picture, width, height);
+            }
+            catch
+            {
+                picture = new Bitmap("cornflake.gif");
+            }
+            this.Size = new Size(width, height);
+            this.Location = new Point(0, 0);
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Image = picture;
+        }
+        public Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(bmp, 0, 0, width, height);
+            }
+            return result;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            valchanged(0, GetAsyncKeyState(Keys.PageUp));
+            if (wd[0] == 1)
+            {
+                this.TopMost = false;
+            }
+            valchanged(1, GetAsyncKeyState(Keys.PageDown));
+            if (wd[1] == 1)
+            {
+                width = Screen.PrimaryScreen.Bounds.Width;
+                height = Screen.PrimaryScreen.Bounds.Height;
+                this.Size = new Size(width, height);
+                this.Location = new Point(0, 0);
+                this.TopMost = true;
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+    }
+}
